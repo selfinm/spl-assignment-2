8acd5c337fd39849fa54f65ee0cc7555bd13150b
selfint <selfint@gmail.com>
2022-12-16 10:43:49 +0200

    test commit

--- src/main/java/bgu/spl/mics/application/services/DeveloperService.java
+++ src/main/java/bgu/spl/mics/application/services/DeveloperService.java
@@ -6,7 +6,8 @@ import bgu.spl.mics.MicroService;
  * developer is responsible for sending the {@link TrainModelEvent},
  * {@link TestModelEvent} and {@link PublishResultsEvent}.
  * In addition, it must sign up for the conference publication broadcasts.
- * This class may not hold references for objects which it is not responsible for.
+ * This class may not hold references for objects which it is not responsible
+ * for.
  *
  * You can add private fields and public methods to this class.
  * You MAY change constructor signatures and even add new public constructors.
@@ -15,6 +16,9 @@ public class DeveloprService extends MicroService {
     public DeveloperService(String name) {
         super("Change_This_Name");
         // TODO Implement this
+
+
+
     }
 
     @Override
