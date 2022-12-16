b85f5cc08b91197b6578a7b6833f278b95a7038d
selfint <selfint@gmail.com>
2022-12-16 10:42:23 +0200

    initial commit

---
+++ src/main/java/bgu/spl/mics/application/objects/Developer.java
@@ -0,0 +1,19 @@
+package bgu.spl.mics.application.objects;
+
+/**
+ * Passive object representing single developer.
+ * Add fields and methods to this class as you see fit (including public methods and constructors).
+ */
+public class Student {
+    
+    enum Position {
+        Intern, Junior, Senior
+    }
+
+    private String name;
+    private String department;
+    private Position status;
+    private int publications;
+    private int papersRead;
+
+}
