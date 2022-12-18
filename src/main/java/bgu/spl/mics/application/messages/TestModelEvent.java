3e3fdf6fddf63846504fe35409c1bcd288a258e9
selfint <selfint@gmail.com>
2022-12-18 20:11:49 +0200

    commit

--- src/main/java/bgu/spl/mics/application/messages/TestModelEvent.java
+++ src/main/java/bgu/spl/mics/application/messages/TestModelEvent.java
@@ -1,20 +1,23 @@
 package bgu.spl.mics.application.messages;
 
 import bgu.spl.mics.Event;
+import bgu.spl.mics.application.objects.Developer;
 import bgu.spl.mics.application.objects.Model;
 
 public class TestModelEvent implements Event<Model> {
     private Model model;
+    private Developer developer;
 
-    public TestModelEvent(Model model) {
+    public TestModelEvent(Model model, Developer developer) {
         this.model = model;
+        this.developer = developer;
+    }
+
+    public Developer getDeveloper() {
+        return developer;
     }
 
     public Model getModel() {
         return model;
-    }
-
-    public void setModel(Model model) {
-        this.model = model;
     }
 }
--- src/test/java/bgu/spl/mics/application/TestTestModelEvent.java
+++ src/test/java/bgu/spl/mics/application/TestTestModelEvent.java
@@ -1,5 +1,7 @@
 package bgu.spl.mics.application;
 
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -7,16 +9,17 @@ import org.junit.Test;
 import bgu.spl.mics.Future;
 import bgu.spl.mics.MessageBusImpl;
 import bgu.spl.mics.application.messages.CloseAllBroadcast;
+import bgu.spl.mics.application.messages.TestModelEvent;
 import bgu.spl.mics.application.messages.TickBroadcast;
-import bgu.spl.mics.application.messages.TrainModelEvent;
-import bgu.spl.mics.application.objects.CPU;
 import bgu.spl.mics.application.objects.Data;
+import bgu.spl.mics.application.objects.Developer;
+import bgu.spl.mics.application.objects.Developer.Status;
 import bgu.spl.mics.application.objects.GPU;
 import bgu.spl.mics.application.objects.Model;
-import bgu.spl.mics.application.services.CPUService;
+import bgu.spl.mics.application.objects.Model.Results;
 import bgu.spl.mics.application.services.GPUService;
 
-public class TestTrainModelEvent {
+public class TestTestModelEvent {
 
     MessageBusImpl m;
 
@@ -29,29 +32,19 @@ public class TestTrainModelEvent {
 
     @Test
     public void testTrainModelEvent() {
-        int cpu1Cores = 1;
-        int cpu2Cores = 2;
         GPU.Type gpuType = GPU.Type.GTX1080;
         int modelSize = 1500;
 
         // build micro services
         GPU gpu = new GPU(gpuType);
         GPUService gpuMs = new GPUService("gs-1", gpu);
-        CPU cpu1 = new CPU(cpu1Cores);
-        CPUService cpu1Ms = new CPUService("cs-1", cpu1);
-        CPU cpu2 = new CPU(cpu2Cores);
-        CPUService cpu2Ms = new CPUService("cs-2", cpu2);
 
         // start microservices
         Thread gpuMsThread = new Thread(gpuMs);
         gpuMsThread.start();
-        Thread cpu1MsThread = new Thread(cpu1Ms);
-        cpu1MsThread.start();
-        Thread cpu2MsThread = new Thread(cpu2Ms);
-        cpu2MsThread.start();
 
         // wait for services to start
-        while (!m.isRegistered(gpuMs, cpu1Ms, cpu2Ms)) {
+        while (!m.isRegistered(gpuMs)) {
             try {
                 Thread.sleep(10);
             } catch (InterruptedException e) {
@@ -60,11 +53,12 @@ public class TestTrainModelEvent {
         }
 
         Model model = new Model("test-model", Data.Type.Tabular, modelSize);
+        Developer developer = new Developer("test-dev", "test", Status.Intern, List.of(model));
 
-        Future<Model> trainedModel = m.sendEvent(new TrainModelEvent(model));
+        Future<Model> testedModel = m.sendEvent(new TestModelEvent(model, developer));
 
         int totalTicks = 0;
-        while (!trainedModel.isDone()) {
+        while (!testedModel.isDone()) {
             m.sendBroadcast(new TickBroadcast());
             totalTicks++;
 
@@ -77,26 +71,11 @@ public class TestTrainModelEvent {
 
         m.sendBroadcast(new CloseAllBroadcast());
 
-        // cpu 1 has 1 core so tacks 32 ticks
-        // cpu 2 finishes before cpu 1
-        // gpu is GTX1080 so tacks 4 ticks
-        // total 36 ticks
-
-        // TODO: we have 2 data races, do we need to solve them?
-        // possible solution: FORCE tick skipping, code will be slower (by 2 ticks each
-        // batch), but predictable
-
-        // if cpu thread notifies cluster that batch is done, after gpu checks, we skip
-        // a tick
-        int maxCpuBeforeGpuTicks = 2;
-        // if gpu thread submits a batch to cluster, after cpu ticks, we skip a tick
-        int maxGpuBeforeCpuTicks = 2;
-
         System.out.println("FINAL TICK COUNT: " + totalTicks);
-        Assert.assertTrue(36 <= totalTicks && totalTicks <= 36 + maxCpuBeforeGpuTicks + maxGpuBeforeCpuTicks);
+        Assert.assertEquals(1, totalTicks);
 
-        Assert.assertEquals(model.getName(), trainedModel.get().getName());
-
+        Assert.assertTrue(testedModel.get().getResults() != null);
+        Assert.assertNotEquals(Results.None, testedModel.get().getResults());
     }
 
 }
